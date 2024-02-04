83e110a687f427852d0d47a79a1227b6545219c6
PapriBiswas2023 <official.p2023@gmail.com>
2024-02-04 20:02:36 +0530

    test-database-connection

--- main.js
+++ main.js
@@ -7,6 +7,21 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+//database connection
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+const db=mongoose.connection;
+db.on('error',(error)=> console.log(error));
+db.once('open',()=>console.log("Connected to the Database!"));
+
 app.get("/",(req,res) => {
     res.send("Hi");
 });
+
+app.listen(PORT,() => {
+    console.log(`Server started at http://localhost:${PORT}`);
+
+});
+
