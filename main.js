0e4ec751c0b0a331281d3b05f6feb9bd23132062
PapriBiswas2023 <official.p2023@gmail.com>
2024-02-05 20:51:14 +0530

    route-model

--- main.js
+++ main.js
@@ -16,6 +16,26 @@ const db=mongoose.connection;
 db.on('error',(error)=> console.log(error));
 db.once('open',()=>console.log("Connected to the Database!"));
 
+//milddlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(session({
+    secret: 'my secret key',
+    saveUninitialized : true,
+    resave: false,
+}));
+
+app.use((req, res, next) =>{
+    res.locals.message =re.session.message;
+    delete req.session.message;
+    next();
+});
+//set templete engine
+app.set('view engine', 'ejs');
+
+// route prefix
+app.use("",require("./routes/routes"));
+
 app.get("/",(req,res) => {
     res.send("Hi");
 });
